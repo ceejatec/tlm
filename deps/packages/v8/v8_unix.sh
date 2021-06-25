cd85b821133b424671291a6a20ba30656cb0aa44
Chris Hillery <ceej@couchbase.com>
2021-06-25 23:28:07 +0000

    CBD-4127: v8 for aarch64 (and gcc10)

--- deps/packages/v8/v8_unix.sh
+++ deps/packages/v8/v8_unix.sh
@@ -3,55 +3,95 @@
 INSTALL_DIR=$1
 PLATFORM=$2
 
+ARCH=`uname -m`
+
 pushd $(dirname $0) > /dev/null
 SCRIPTPATH=$(pwd -P)
 popd > /dev/null
 
 DEPS=/tmp/deps
-rm -rf ${DEPS}
 mkdir -p ${DEPS}
 
-# Download cbdep to get python2 (still required for this build sadly).
-CBDEP_TOOL_VER=0.9.15
-MINICONDA_VER=4.7.12.1
+# We assume a reasonable python2 is available for aarch64 builds.
+# Can't fall back to miniconda there because miniconda doesn't have
+# aarch64 versions of python2.
+if [ "${ARCH}" = "x86_64" ]; then
+    # Download cbdep to get python2 (still required for this build sadly).
+    CBDEP_TOOL_VER=0.9.15
+    MINICONDA_VER=4.7.12.1
 
-# Download cbdep, unless it's already available in the local .cbdepscache
-OPSYS=$(uname -s | tr "[:upper:]" "[:lower:]")
-CBDEP_BIN_CACHE=/home/couchbase/.cbdepscache/cbdep/${CBDEP_TOOL_VER}/cbdep-${CBDEP_TOOL_VER}-${OPSYS}
+    # Download cbdep, unless it's already available in the local .cbdepscache
+    OPSYS=$(uname -s | tr "[:upper:]" "[:lower:]")
+    CBDEP_BIN_CACHE=/home/couchbase/.cbdepscache/cbdep/${CBDEP_TOOL_VER}/cbdep-${CBDEP_TOOL_VER}-${OPSYS}
 
-if [[ -f ${CBDEP_BIN_CACHE} ]]; then
-    cp ${CBDEP_BIN_CACHE} /tmp/cbdep
-else
-    CBDEP_URL=https://packages.couchbase.com/cbdep/${CBDEP_TOOL_VER}/cbdep-${CBDEP_TOOL_VER}-${OPSYS}
-    curl -o /tmp/cbdep ${CBDEP_URL}
+    if [[ -f ${CBDEP_BIN_CACHE} ]]; then
+        cp ${CBDEP_BIN_CACHE} /tmp/cbdep
+    else
+        CBDEP_URL=https://packages.couchbase.com/cbdep/${CBDEP_TOOL_VER}/cbdep-${CBDEP_TOOL_VER}-${OPSYS}
+        curl -o /tmp/cbdep ${CBDEP_URL}
+    fi
+
+    chmod +x /tmp/cbdep
+
+    # Use cbdep to install miniconda2. Add to PATH *last* (so it only adds
+    # python2, not overriding anything). Also add to LD_LIBRARY_PATH as the
+    # "vpython" script the build uses creates a copy of "python2" but doesn't
+    # copy libpython2 as well.
+    # On macosx, we need to use python from miniconda; otherwise, gclient will
+    # fail due to SIP.  Creating an alias doesn't seem to work; have to add
+    # miniconda2 in the front of PATH.  ubuntu20 comes with python3, v8 doesn't
+    # work well with python3, put miniconda2 in the front as well.
+
+    /tmp/cbdep install -d ${DEPS} miniconda2 ${MINICONDA_VER}
+    export PATH_ORG=${PATH}
+    export LD_LIBRARY_PATH=${DEPS}/miniconda2-${MINICONDA_VER}/lib
+
+    if [[ $PLATFORM != "macosx" && $PLATFORM != "ubuntu20.04" && $PLATFORM != "ubuntu18.04" ]]; then
+        export PATH=${PATH_ORG}:${DEPS}/miniconda2-${MINICONDA_VER}/bin
+    else
+        export PATH=${DEPS}/miniconda2-${MINICONDA_VER}/bin:${PATH_ORG}
+    fi
 fi
 
-chmod +x /tmp/cbdep
-
-# Use cbdep to install miniconda2. Add to PATH *last* (so it only adds
-# python2, not overriding anything). Also add to LD_LIBRARY_PATH as the
-# "vpython" script the build uses creates a copy of "python2" but doesn't
-# copy libpython2 as well.
-# On macosx, we need to use python from miniconda; otherwise, gclient will
-# fail due to SIP.  Creating an alias doesn't seem to work; have to add
-# miniconda2 in the front of PATH.  ubuntu20 comes with python3, v8 doesn't
-# work well with python3, put miniconda2 in the front as well.
-
-/tmp/cbdep install -d ${DEPS} miniconda2 ${MINICONDA_VER}
-export PATH_ORG=${PATH}
-export LD_LIBRARY_PATH=${DEPS}/miniconda2-${MINICONDA_VER}/lib
-
-if [[ $PLATFORM != "macosx" && $PLATFORM != "ubuntu20.04" && $PLATFORM != "ubuntu18.04" ]]; then
-    export PATH=${PATH_ORG}:${DEPS}/miniconda2-${MINICONDA_VER}/bin
-else
-    export PATH=${DEPS}/miniconda2-${MINICONDA_VER}/bin:${PATH_ORG}
-fi
-
-# Get Google's depot_tools; checkout from October 18th, 2018,
-# which worked for the SuSE platforms on the last build.
+# Get Google's depot_tools; lock to moderately recent build (as of June 2021)
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-pushd depot_tools && git checkout 93277a7 && popd
+pushd depot_tools && git checkout e319aba2 && popd
 export PATH=$(pwd)/depot_tools:$PATH
+
+# Do NOT use their bundled python binary
+export VPYTHON_BYPASS="manually managed python not supported by chrome operations"
+
+# Build Ninja
+pushd ${DEPS}
+if [ ! -d ninja ]; then
+    mkdir ninja
+    pushd ninja
+    git clone https://github.com/ninja-build/ninja.git -b v1.8.2
+    cd ninja
+    ./configure.py --bootstrap
+    popd
+fi
+export PATH=$(pwd)/ninja/ninja:$PATH
+popd
+
+# Build an older version of GN that's still able to build v8 8.3
+# (later versions cannot). We have to build our own on at least
+# some platforms either due to Google's depot_tools' gn using too
+# new a glibc, or else not working on aarch64. We also have to
+# force it to use our compiler.
+pushd ${DEPS}
+if [ ! -d gn ]; then
+    mkdir gn
+    pushd gn
+    git clone https://gn.googlesource.com/gn
+    cd gn
+    git checkout 5ed3c
+    CC=$(which gcc) CXX=$(which g++) python build/gen.py
+    ninja -C out
+    popd
+fi
+export PATH=$(pwd)/gn/gn/out:$PATH
+popd
 
 # Set up gclient config for tag to pull for v8, then do sync
 # (this handles the 'fetch v8' done by the usual process)
@@ -68,26 +108,47 @@ EOF
 
 gclient sync
 
-# Apply change to enable RPATH (runpath) for libraries/binaries on Linux
+# Several patches and tweaks for Linux builds
 if [[ $PLATFORM != "macosx" ]]; then
+    # Apply change to enable RPATH (runpath) for libraries/binaries on Linux
     pushd v8/build
-    git apply $SCRIPTPATH/v8_linux_runpath.patch
+    git apply $SCRIPTPATH/linux_patches/v8_linux_runpath.patch
     popd
-fi
 
-#download older version of gn for centos7
-#latest requires glibc 2.18, centos7 comes with 2.17
-if [[ $PLATFORM == "centos7" ]]; then
-    pushd v8/buildtools/linux64
-    mv gn gn.org
-    curl -L -o gn.zip https://chrome-infra-packages.appspot.com/dl/gn/gn/linux-amd64/+/jCF636-ci00rA64wJkdAPZx3NrIvafSvZ_bajkcCMbUC
-    unzip gn.zip
+    # Necessary for building on aarch64.
+    # https://gist.github.com/Adenilson/de40a2de85d60c9d30bd79fbcb4ad4a2
+    pushd v8/third_party/zlib
+    git apply $SCRIPTPATH/linux_patches/zlib_aarch64.patch
+    popd
+
+    # Necessary for building with gcc10.
+    # https://chromium-review.googlesource.com/c/v8/v8/+/2248196/2/src/base/template-utils.h
+    pushd v8
+    git apply $SCRIPTPATH/linux_patches/iosfwd_gcc10.patch
+    popd
+
+    # Also necessary with gcc10 because it has a number of new warnings.
+    pushd v8/build
+    git apply $SCRIPTPATH/linux_patches/no_werror_gcc10.patch
+    popd
+
+    # Set up all the weird tool aliases that gn expects
+    pushd ${DEPS}
+    for tool in gcc g++ ar readelf nm; do
+	ln -s $(which ${tool}) $(uname -m)-linux-gnu-${tool}
+    done
+    export PATH=$(pwd):$PATH
     popd
 fi
 
 # Actual v8 configure and build steps - we build debug and release.
+if [ "${ARCH}" = "aarch64" ]; then
+    TARGET_CPU=arm64
+else
+    TARGET_CPU=x64
+fi
 cd v8
-V8_ARGS='target_cpu="x64" is_component_build=true v8_enable_backtrace=true v8_use_external_startup_data=false use_custom_libcxx=false v8_enable_pointer_compression=false'
+V8_ARGS="target_cpu=\"${TARGET_CPU}\" is_clang=false use_sysroot=false is_component_build=true v8_enable_backtrace=true v8_use_external_startup_data=false use_custom_libcxx=false v8_enable_pointer_compression=false"
 
 gn gen out.gn/x64.release --args="$V8_ARGS is_debug=false"
 ninja -C out.gn/x64.release
